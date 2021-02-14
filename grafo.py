9eadf7d5ecf6f90ca543e81d5e6811fd309f6345
Joaquin Prada <joaquiprada@gmail.com>
2021-02-14 18:11:30 -0300

    grafo.py esqueleto

---
+++ grafo.py
@@ -0,0 +1,37 @@
+class Grafo:
+
+    def __init__() -> None:
+        pass
+
+    def __str__(self) -> str:
+        pass
+
+    def __iter__(self):
+        pass
+
+    def agregar_vertice(self, Vertice):
+        pass
+
+    def borrar_vertice():
+        pass
+
+    def agregar_arista():
+        pass
+
+    def borrar_arista():
+        pass
+
+    def son_adyacentes(v1, v2):
+        pass
+
+    def existe_vertice(id):
+        pass
+
+    def obtener_vertice_random():
+        pass
+
+    def vertices(self):
+        pass
+
+    def adyacentes(self, v):
+        pass
