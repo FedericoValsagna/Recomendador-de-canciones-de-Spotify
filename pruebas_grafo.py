86f0d33e93199057f8f1a5d15f87da912b81c371
Joaquin Prada <joaquiprada@gmail.com>
2021-02-15 00:32:36 -0300

    2 pruebas mas

--- pruebas_grafo.py
+++ pruebas_grafo.py
@@ -10,13 +10,16 @@ def prueba_grafo_agregar_borrar_vertices():
 
     grafo.agregar_vertice("1", None)
     grafo.agregar_vertice("2", None)
+
     assert(grafo.obtener_cantidad() == 2)
     for v in grafo.obtener_vertices():
         assert(v == "1" or v == "2")
+
     grafo.borrar_vertice("1")
     assert(grafo.obtener_cantidad() == 1)
     for v in grafo.obtener_vertices():
         assert(v == "2")
+
     grafo.borrar_vertice("2")
     assert(grafo.obtener_cantidad() == 0)
 
@@ -76,12 +79,50 @@ def prueba_grafo_aristas_dirigido():
     assert(not grafo.es_adyacente("4","2"))
     assert(not grafo.es_adyacente("4","3"))
 
+def prueba_grafo_obtener_vertices():
+    grafo = Grafo(True)
+
+    grafo.agregar_vertice("1", None)
+    grafo.agregar_vertice("2", None)
+    grafo.agregar_vertice("3", None)
+    grafo.agregar_vertice("4", None)
+    grafo.agregar_arista("1","2")
+    grafo.agregar_arista("2","3")
+    grafo.agregar_arista("3","2")
+    grafo.agregar_arista("3","4")
+    grafo.agregar_arista("4","1")
+
+    vertices = grafo.obtener_vertices()
+    for v in vertices:
+        assert(grafo.existe_vertice(v))
+
+def prueba_grafo_vertice_aleatorio():
+    grafo = Grafo(True)
+
+    grafo.agregar_vertice("1", None)
+    grafo.agregar_vertice("2", None)
+    grafo.agregar_vertice("3", None)
+    grafo.agregar_vertice("4", None)
+    grafo.agregar_arista("1","2")
+    grafo.agregar_arista("2","3")
+    grafo.agregar_arista("3","2")
+    grafo.agregar_arista("3","4")
+    grafo.agregar_arista("4","1")
+
+    iteraciones = 10
+
+    for _ in range(iteraciones):
+        assert(grafo.existe_vertice(grafo.obtener_vertice_random()))
+
+    
 
 def main():
     prueba_grafo_crear()
     prueba_grafo_agregar_borrar_vertices()
     prueba_grafo_aristas_no_dirigido()
     prueba_grafo_aristas_dirigido()
+    prueba_grafo_obtener_vertices()
+    prueba_grafo_vertice_aleatorio()
     print("Todo OK :)")
 
 main()
