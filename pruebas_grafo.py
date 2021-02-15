b8458c0370d13916d1cbf8de40e512dc26a24c40
FedericoValsagna <fvalsagna@fi.uba.ar>
2021-02-15 01:09:14 -0300

    Agregadas pruebas de obtener peso

--- pruebas_grafo.py
+++ pruebas_grafo.py
@@ -23,7 +23,7 @@ def prueba_grafo_agregar_borrar_vertices():
     grafo.borrar_vertice("2")
     assert(grafo.obtener_cantidad() == 0)
 
-def prueba_grafo_aristas_no_dirigido():
+def prueba_grafo_aristas_adyacencia_no_dirigido():
     grafo = Grafo()
 
     grafo.agregar_vertice("1", None)
@@ -49,8 +49,12 @@ def prueba_grafo_aristas_no_dirigido():
     assert(grafo.es_adyacente("4","1"))
     assert(not grafo.es_adyacente("4","2"))
     assert(not grafo.es_adyacente("4","3"))
+    
+    grafo.borrar_arista("2","3")
+    assert(not grafo.es_adyacente("2","3"))
+    assert(not grafo.es_adyacente("3","2"))
 
-def prueba_grafo_aristas_dirigido():
+def prueba_grafo_aristas_adyacencia_dirigido():
     grafo = Grafo(es_dirigido=True)
 
     grafo.agregar_vertice("1", None)
@@ -78,6 +82,55 @@ def prueba_grafo_aristas_dirigido():
     assert(grafo.es_adyacente("4","1"))
     assert(not grafo.es_adyacente("4","2"))
     assert(not grafo.es_adyacente("4","3"))
+
+    grafo.borrar_arista("2","3")
+    assert(not grafo.es_adyacente("2","3"))
+    assert(grafo.es_adyacente("3","2"))
+
+def prueba_grafo_aristas_obtener_peso_no_dirigido():
+    grafo = Grafo()
+
+    grafo.agregar_vertice("1", None)
+    grafo.agregar_vertice("2", None)
+    grafo.agregar_vertice("3", None)
+    grafo.agregar_vertice("4", None)
+    grafo.agregar_vertice("5", None)
+    grafo.agregar_arista("1","2",4)
+    grafo.agregar_arista("2","3",3)
+    grafo.agregar_arista("4","1",-547)
+    grafo.agregar_arista("3","5", 0)
+    grafo.agregar_arista("1","5")
+
+    assert(grafo.obtener_peso("1","2") == 4)
+    assert(grafo.obtener_peso("2","1") == 4)
+    assert(grafo.obtener_peso("2","3") == 3)
+    assert(grafo.obtener_peso("3","2") == 3)
+    assert(grafo.obtener_peso("4","1") == -547)
+    assert(grafo.obtener_peso("1","4") == -547)
+    assert(grafo.obtener_peso("3","5") == 0)
+    assert(grafo.obtener_peso("5","3") == 0)
+    assert(grafo.obtener_peso("1","5") == 0)
+    assert(grafo.obtener_peso("5","1") == 0)
+
+def prueba_grafo_aristas_obtener_peso_dirigido():
+    grafo = Grafo(es_dirigido= True)
+
+    grafo.agregar_vertice("1", None)
+    grafo.agregar_vertice("2", None)
+    grafo.agregar_vertice("3", None)
+    grafo.agregar_vertice("4", None)
+    grafo.agregar_arista("1","2",4)
+    grafo.agregar_arista("2","3",3)
+    grafo.agregar_arista("3","2",0)
+    grafo.agregar_arista("3","4")
+    grafo.agregar_arista("4","1",-547)
+
+    assert(grafo.obtener_peso("1","2") == 4)
+    assert(grafo.obtener_peso("2","3") == 3)
+    assert(grafo.obtener_peso("3","2") == 0)
+    assert(grafo.obtener_peso("4","1") == -547)
+    assert(grafo.obtener_peso("3","4") == 0)
+
 
 def prueba_grafo_obtener_adyacentes():
     grafo = Grafo(es_dirigido=True)
@@ -136,8 +189,10 @@ def prueba_grafo_vertice_aleatorio():
 def main():
     prueba_grafo_crear()
     prueba_grafo_agregar_borrar_vertices()
-    prueba_grafo_aristas_no_dirigido()
-    prueba_grafo_aristas_dirigido()
+    prueba_grafo_aristas_adyacencia_no_dirigido()
+    prueba_grafo_aristas_adyacencia_dirigido()
+    prueba_grafo_aristas_obtener_peso_no_dirigido()
+    prueba_grafo_aristas_obtener_peso_dirigido()
     prueba_grafo_obtener_adyacentes()
     prueba_grafo_obtener_vertices()
     prueba_grafo_vertice_aleatorio()
