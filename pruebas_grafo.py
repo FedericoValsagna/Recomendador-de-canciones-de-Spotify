bc0975547121673ca0efe4485c93dd001aa09caf
FedericoValsagna <fvalsagna@fi.uba.ar>
2021-02-15 00:23:29 -0300

    Agregadas pruebas de adayencia

--- pruebas_grafo.py
+++ pruebas_grafo.py
@@ -20,10 +20,68 @@ def prueba_grafo_agregar_borrar_vertices():
     grafo.borrar_vertice("2")
     assert(grafo.obtener_cantidad() == 0)
 
+def prueba_grafo_aristas_no_dirigido():
+    grafo = Grafo()
+
+    grafo.agregar_vertice("1", None)
+    grafo.agregar_vertice("2", None)
+    grafo.agregar_vertice("3", None)
+    grafo.agregar_vertice("4", None)
+    grafo.agregar_arista("1","2")
+    grafo.agregar_arista("2","3")
+    grafo.agregar_arista("4","1")
+
+    assert(grafo.es_adyacente("1","2"))
+    assert(not grafo.es_adyacente("1","3"))
+    assert(grafo.es_adyacente("1","4"))
+
+    assert(grafo.es_adyacente("2","1"))
+    assert(grafo.es_adyacente("2","3"))
+    assert(not grafo.es_adyacente("2","4"))
+
+    assert(not grafo.es_adyacente("3","1"))
+    assert(grafo.es_adyacente("3","2"))
+    assert(not grafo.es_adyacente("3","4"))
+
+    assert(grafo.es_adyacente("4","1"))
+    assert(not grafo.es_adyacente("4","2"))
+    assert(not grafo.es_adyacente("4","3"))
+
+def prueba_grafo_aristas_dirigido():
+    grafo = Grafo(True)
+
+    grafo.agregar_vertice("1", None)
+    grafo.agregar_vertice("2", None)
+    grafo.agregar_vertice("3", None)
+    grafo.agregar_vertice("4", None)
+    grafo.agregar_arista("1","2")
+    grafo.agregar_arista("2","3")
+    grafo.agregar_arista("3","2")
+    grafo.agregar_arista("3","4")
+    grafo.agregar_arista("4","1")
+
+    assert(grafo.es_adyacente("1","2"))
+    assert(not grafo.es_adyacente("1","3"))
+    assert(not grafo.es_adyacente("1","4"))
+
+    assert(not grafo.es_adyacente("2","1"))
+    assert(grafo.es_adyacente("2","3"))
+    assert(not grafo.es_adyacente("2","4"))
+
+    assert(not grafo.es_adyacente("3","1"))
+    assert(grafo.es_adyacente("3","2"))
+    assert(grafo.es_adyacente("3","4"))
+
+    assert(grafo.es_adyacente("4","1"))
+    assert(not grafo.es_adyacente("4","2"))
+    assert(not grafo.es_adyacente("4","3"))
+
 
 def main():
     prueba_grafo_crear()
     prueba_grafo_agregar_borrar_vertices()
+    prueba_grafo_aristas_no_dirigido()
+    prueba_grafo_aristas_dirigido()
     print("Todo OK :)")
 
 main()
