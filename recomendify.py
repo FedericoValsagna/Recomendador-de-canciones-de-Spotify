2eb2f6929b883b450ce0bb42e9afd52a70ad7692
Joaquin Prada <joaquiprada@gmail.com>
2021-02-21 18:47:12 -0300

    Lectura de comandos

--- recomendify.py
+++ recomendify.py
@@ -10,6 +10,53 @@ PLAYLIST_ID = 4
 PLAYLIST_NAME = 5
 GENRES = 6
 
+COMANDOS = {"camino", "mas_importantes", "recomendacion", "ciclo", "rango", "clustering"}
+
+CAMINO = "camino"
+MAS_IMPORTANTES = "mas_importantes"
+RECOMENDACION = "recomendacion"
+CICLO = "ciclo"
+RANGO = "rango"
+CLUSTERING = "clustering"
+
+
+
+
+def generar_camino(grafo1, cancion1, cancion2):
+    print(cancion1, cancion2)
+
+
+def ejecutar_comando(comando, parametros, grafo1, grafo2):
+    parametros = parametros.split(" >>>> ")
+    if comando == CAMINO:
+        if len(parametros) != 2:
+            print("Cantidad de parametros erronea")
+            return False
+        cancion1 = parametros[0]
+        cancion2 = parametros[1]
+        if(not grafo1.existe_vertice(cancion1) or not grafo1.existe_vertice(cancion2)):
+            print("Tanto el origen como el destino deben ser canciones.")
+            return False
+        generar_camino(grafo1, cancion1, cancion2)
+
+def verificar_comando(comando):
+    return comando in COMANDOS
+
+def procesar_comando(grafo1, grafo2):
+        linea = "First"
+        while linea:
+            linea = sys.stdin.readline().rstrip()
+            linea = linea.split(" ")    
+            if not verificar_comando(linea[0]):
+                print("El comando ingresado no es valido")
+                continue
+            if len(linea) < 2:
+                print("Por favor ingrese parametros con su comando")
+                continue
+            comando = linea[0]
+            linea = " ".join(linea[1:])
+            ejecutar_comando(comando, linea, grafo1, grafo2)
+
 def procesar_archivo(ruta, grafo1, grafo2):
     with open(ruta, encoding = "utf8") as archivo:
         archivo.readline() # 2 veces para no usar el header del archivo
@@ -19,22 +66,21 @@ def procesar_archivo(ruta, grafo1, grafo2):
         while linea:     # linea != ''
             linea = linea.rstrip('\n')
             linea = linea.split('\t')
-
-            playlist = (linea[PLAYLIST_ID], linea[PLAYLIST_NAME])
+            
+            playlist = linea[PLAYLIST_ID]
             if playlist_actual != playlist:
                 canciones_anteriores.clear()
                 playlist_actual = playlist
             
-            procesar_linea(linea, grafo1, grafo2, canciones_anteriores, playlist, playlist_actual)
+            procesar_linea(linea, grafo1, grafo2, canciones_anteriores)
             linea = archivo.readline()
 
-    
-    
-def procesar_linea(linea, grafo1, grafo2, canciones_anteriores, playlist, playlist_actual):
+def procesar_linea(linea, grafo1, grafo2, canciones_anteriores):
     cancion = " - ".join((linea[TRACK_NAME], linea[ARTIST]))
     usuario = linea[USER_ID]
+    playlist = (linea[PLAYLIST_ID], linea[PLAYLIST_NAME])
     crear_grafo1(grafo1, linea, cancion, usuario, playlist)
-    crear_grafo2(grafo2, cancion, playlist, canciones_anteriores, playlist_actual)
+    crear_grafo2(grafo2, cancion, playlist, canciones_anteriores)
 
 def crear_grafo1(grafo1, linea, cancion, usuario, playlist):
     if not grafo1.existe_vertice(usuario):
@@ -49,7 +95,7 @@ def crear_grafo1(grafo1, linea, cancion, usuario, playlist):
     lista_playlists = grafo1.obtener_peso(usuario, cancion)
     lista_playlists.append(playlist)
 
-def crear_grafo2(grafo2, cancion, playlist, canciones_anteriores, playlist_actual):
+def crear_grafo2(grafo2, cancion, playlist, canciones_anteriores):
     if not grafo2.existe_vertice(cancion):
         grafo2.agregar_vertice(cancion)
 
@@ -61,13 +107,17 @@ def crear_grafo2(grafo2, cancion, playlist, canciones_anteriores, playlist_actua
         lista_playlists.append(playlist)
     canciones_anteriores.append(cancion)
 
-
 def main():
     ruta_archivo = sys.argv[1]
 
     grafo1 = Grafo()
     grafo2 = Grafo()
+
+    print("Cargando datos...")
     procesar_archivo(ruta_archivo, grafo1, grafo2)
+
+    print("Esperando comandos: ")
+    procesar_comando(grafo1, grafo2)
     
 
 
