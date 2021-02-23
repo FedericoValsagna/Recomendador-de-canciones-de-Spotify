fac2f7e8d3ee23b34f06ccba5ee53b5de22bd1f8
Joaquin Prada <joaquiprada@gmail.com>
2021-02-22 21:02:16 -0300

    Creado clase recomendify

---
+++ main.py
@@ -0,0 +1,59 @@
+from recomendify import Recomendify
+from grafo import Grafo 
+import sys
+from constantes import *
+from funciones_recomendify import *
+
+
+def ejecutar_comando(comando, parametros, recomendify):
+    if comando == CAMINO:
+        recomendify.camino_mas_corto(parametros)
+
+    if comando == MAS_IMPORTANTES:
+        recomendify.canciones_mas_importantes(parametros)
+    
+    if comando == RECOMENDACION:
+        recomendify.recomendacion(parametros)
+
+    if comando == CICLO:
+        recomendify.ciclo_de_n_canciones(parametros)
+
+    if comando == RANGO:
+        recomendify.todas_en_rango(parametros)
+    
+    if comando == CLUSTERING:
+        recomendify.coeficiente_de_clustering(parametros)
+
+def verificar_comando(comando):
+    return comando in COMANDOS
+
+def procesar_comando(recomendify):
+        linea = "First"
+        while linea:
+            linea = sys.stdin.readline().rstrip()
+            if not linea:
+                break
+            linea = linea.split(" ")
+            if not verificar_comando(linea[0]):
+                print(ERROR_COMANDO)
+                continue
+            if len(linea) < 2:
+                print(ERROR_PARAMETROS_NULO)
+                continue
+            comando = linea[0]
+            linea = " ".join(linea[1:])
+            parametros = linea.split(SEPARADOR)
+            ejecutar_comando(comando, parametros, recomendify)
+
+def main():
+    ruta_archivo = sys.argv[1]
+
+    print("Cargando datos...")
+    recomendify = Recomendify(ruta_archivo)
+
+    print("Esperando comandos: ")
+    procesar_comando(recomendify)
+    
+
+if __name__ == "__main__":
+    main()
--- recomendify.py
+++ recomendify.py
@@ -1,110 +1,85 @@
-from grafo import Grafo 
-import sys
+from grafo import Grafo
 from constantes import *
-from funciones_recomendify import *
+from funciones_grafo import *
 
-def ejecutar_comando(comando, parametros, grafo1, grafo2):
-    if comando == CAMINO:
-        camino_mas_corto(grafo1, parametros)
+class Recomendify:
 
-    if comando == MAS_IMPORTANTES:
-        pass
+    def __init__(self, ruta_archivo):
+        self.ruta_archivo = ruta_archivo
+        self.grafo1 = Grafo()
+        self.grafo2 = Grafo()
+        self._canciones_anteriores = []
+        self._playlist_actual = None
+        self._procesar_archivo()
     
-    if comando == RECOMENDACION:
-        pass
-
-    if comando == CICLO:
-        pass
-
-    if comando == RANGO:
-        pass
-    
-    if comando == CLUSTERING:
-        pass
-
-def verificar_comando(comando):
-    return comando in COMANDOS
-
-def procesar_comando(grafo1, grafo2):
-        linea = "First"
-        while linea:
-            linea = sys.stdin.readline().rstrip()
-            if not linea:
-                break
-            linea = linea.split(" ")
-            if not verificar_comando(linea[0]):
-                print(ERROR_COMANDO)
-                continue
-            if len(linea) < 2:
-                print(ERROR_PARAMETROS_NULO)
-                continue
-            comando = linea[0]
-            linea = " ".join(linea[1:])
-            parametros = linea.split(SEPARADOR)
-            ejecutar_comando(comando, parametros, grafo1, grafo2)
-
-def procesar_archivo(ruta, grafo1, grafo2):
-    with open(ruta, encoding = "utf8") as archivo:
-        archivo.readline() # 2 veces para no usar el header del archivo
-        linea = archivo.readline()
-        canciones_anteriores = []
-        playlist_actual = None
-        while linea:     # linea != ''
-            linea = linea.rstrip('\n')
-            linea = linea.split('\t')
-            
-            playlist = linea[PLAYLIST_ID]
-            if playlist_actual != playlist:
-                canciones_anteriores.clear()
-                playlist_actual = playlist
-            
-            procesar_linea(linea, grafo1, grafo2, canciones_anteriores)
+    def _procesar_archivo(self):
+        with open(self.ruta_archivo, encoding = "utf8") as archivo:
+            archivo.readline() # 2 veces para no usar el header del archivo
             linea = archivo.readline()
+            while linea:
+                linea = linea.rstrip('\n')
+                linea = linea.split('\t')
+                self._procesar_linea(linea)
+                linea = archivo.readline()
 
-def procesar_linea(linea, grafo1, grafo2, canciones_anteriores):
-    cancion = " - ".join((linea[TRACK_NAME], linea[ARTIST]))
-    usuario = linea[USER_ID]
-    playlist = (linea[PLAYLIST_ID], linea[PLAYLIST_NAME])
-    crear_grafo1(grafo1, linea, cancion, usuario, playlist)
-    crear_grafo2(grafo2, cancion, playlist, canciones_anteriores)
+    def _procesar_linea(self, linea):
+        cancion = " - ".join((linea[TRACK_NAME], linea[ARTIST]))
+        usuario = linea[USER_ID]
+        playlist = (linea[PLAYLIST_ID], linea[PLAYLIST_NAME])
+        self._crear_grafo1(linea, cancion, usuario, playlist)
+        self._crear_grafo2(cancion, playlist)
 
-def crear_grafo1(grafo1, linea, cancion, usuario, playlist):
-    if not grafo1.existe_vertice(usuario):
-        grafo1.agregar_vertice(usuario)
-    
-    if not grafo1.existe_vertice(cancion):
-        grafo1.agregar_vertice(cancion)
+    def _crear_grafo1(self, linea, cancion, usuario, playlist):
+        if not self.grafo1.existe_vertice(usuario):
+            self.grafo1.agregar_vertice(usuario)
 
-    if not grafo1.es_adyacente(usuario, cancion):
-        grafo1.agregar_arista(usuario, cancion, [])
-    
-    lista_playlists = grafo1.obtener_peso(usuario, cancion)
-    lista_playlists.append(playlist)
+        if not self.grafo1.existe_vertice(cancion):
+            self.grafo1.agregar_vertice(cancion)
 
-def crear_grafo2(grafo2, cancion, playlist, canciones_anteriores):
-    if not grafo2.existe_vertice(cancion):
-        grafo2.agregar_vertice(cancion)
+        if not self.grafo1.es_adyacente(usuario, cancion):
+            self.grafo1.agregar_arista(usuario, cancion, [])
 
-    for track in canciones_anteriores:
-        if not grafo2.es_adyacente(cancion, track):
-            grafo2.agregar_arista(cancion, track, [])
-        
-        lista_playlists = grafo2.obtener_peso(cancion, track)
+        lista_playlists = self.grafo1.obtener_peso(usuario, cancion)
         lista_playlists.append(playlist)
-    canciones_anteriores.append(cancion)
 
-def main():
-    ruta_archivo = sys.argv[1]
+    def _crear_grafo2(self, cancion, playlist):
+        if not self.grafo2.existe_vertice(cancion):
+            self.grafo2.agregar_vertice(cancion)
 
-    grafo1 = Grafo()
-    grafo2 = Grafo()
+        if (self._playlist_actual != playlist):
+            self._canciones_anteriores.clear()
+            self._playlist_actual = playlist
 
-    print("Cargando datos...")
-    procesar_archivo(ruta_archivo, grafo1, grafo2)
+        for track in self._canciones_anteriores:
+            if not self.grafo2.es_adyacente(cancion, track):
+                self.grafo2.agregar_arista(cancion, track, [])
 
-    print("Esperando comandos: ")
-    procesar_comando(grafo1, grafo2)
-    
+            lista_playlists = self.grafo2.obtener_peso(cancion, track)
+            lista_playlists.append(playlist)
+        self._canciones_anteriores.append(cancion)
 
-if __name__ == "__main__":
-    main()
+    def camino_mas_corto(self, parametros):
+        if len(parametros) != 2:
+            print(ERROR_PARAMETROS_CANTIDAD)
+            return False
+        cancion1 = parametros[0]
+        cancion2 = parametros[1]
+        if(not self.grafo1.existe_vertice(cancion1) or not self.grafo1.existe_vertice(cancion2)):
+            print(ERROR_CANCIONES)
+            return False
+        camino = generar_camino(self.grafo1, cancion1, cancion2)
+
+    def canciones_mas_importantes(self, parametros):
+        pass
+
+    def recomendacion(self, parametros):
+        pass
+
+    def ciclo_de_n_canciones(self, parametros):
+        pass
+
+    def todas_en_rango(self, parametros):
+        pass
+
+    def coeficiente_de_clustering(self, parametros):
+        pass
