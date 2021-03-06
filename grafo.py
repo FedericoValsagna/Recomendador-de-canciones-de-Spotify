cc1ca13e711b2409ca109026b96bada8371ce037
root <root@DESKTOP-O4UNBFE.localdomain>
2021-03-06 00:22:57 -0300

    Documentada la clase grafo

--- grafo.py
+++ grafo.py
@@ -23,6 +23,12 @@ class Grafo:
         return grafo
         
     def agregar_vertice(self, id):
+        """
+        Crea un vertice en el grafo con el ID que se pase por parametro.
+        Si el vertice ya existe se levanta una exepción.
+
+        No retorna ningun valor.
+        """
         if id in self.vertices:
             raise ValueError("El vertice ya existe")
         v = Vertice()
@@ -30,6 +36,12 @@ class Grafo:
         self.cantidad_vertices += 1
  
     def borrar_vertice(self, id):
+        """
+        Borra el vertice correspondiente al ID pasado por parametro. 
+        Si el vertice no existe se levanta una exepeción.
+
+        No retorna ningun valor.
+        """
         if id not in self.vertices:
             raise ValueError("El vertice no existe")
         if self.es_dirigido:
@@ -44,6 +56,16 @@ class Grafo:
         self.cantidad_vertices -= 1
         
     def agregar_arista(self, id_1, id_2, peso=0):
+        """
+        Se crea una arista que une a los vertices pasados por parametros. Si el grafo es dirigido la arista va del primer vertice al segundo.
+        Si no existe alguno de los vertices se levanta una exepción. Si la arista ya existe se levanta una exepción. 
+        
+        No retorna ningun valor.
+
+
+        Parametro adicional: Peso de la arista, peso predeterminado: 0
+
+        """
         if id_1 not in self.vertices:
             raise ValueError("El vertice 1 no existe")
         if id_2 not in self.vertices:
@@ -61,6 +83,12 @@ class Grafo:
             self.vertices[id_2].adyacentes[id_1] = peso
     
     def borrar_arista(self, id_1, id_2):
+        """
+        Borra la arista entre los vertices pasados por parametros. Si el grafo es dirigido la arista va del primer vertice al segundo. 
+        Si no existe alguno de los vertices se levanta una exepción. Si no existe la arista que se desea borrar se levanta una exepción.
+        
+        Devuelve el peso de la arista.
+        """
         if id_1 not in self.vertices:
             raise ValueError("El vertice 1 no existe")
         if id_2 not in self.vertices:
@@ -79,6 +107,12 @@ class Grafo:
         return peso
 
     def cambiar_peso(self, id_1, id_2, peso):
+        """
+        Se recibe por parametro ambos vertices que son conectados por la arista y el peso de la arista. Si el grafo es dirigido la arista va del primer vertice al segundo.
+        Si no existe alguno de los vertices se levanta una exepción. Si no existe la arista se levanta una exepción.
+
+        No retorna ningun valor la función.
+        """
         if id_1 not in self.vertices:
             raise ValueError("El vertice 1 no existe")
         if id_2 not in self.vertices:
@@ -93,6 +127,12 @@ class Grafo:
             self.vertices[id_2].adyacentes[id_1] = peso
 
     def es_adyacente(self, id_1, id_2):
+        """
+        Se recibe por parametro 2 vertices y se retorna si el primero es adyacente al segundo.
+        Si alguno de ambos vertices no existe se levanta una exepción.
+
+        Se retorna un booleano indicando si existe o no la adyacencia.
+        """
         if id_1 not in self.vertices:
             raise ValueError("El vertice 1 no existe")
         if id_2 not in self.vertices:
@@ -100,12 +140,24 @@ class Grafo:
         return id_2 in self.vertices[id_1].adyacentes
 
     def existe_vertice(self, id):
+        """
+        Se recibe por parametro un vertice y se retorna un booleano indicando si existe o no.
+        """
         return id in self.vertices
 
     def obtener_vertice_random(self):
+        """
+        Devuelve un vertice aleatoreo.
+        """
         return random.choice(list(self.vertices))
 
     def obtener_peso(self, id_1, id_2):
+        """
+        Recibe 2 vertices por parametro y devuelve el peso de la arista que conecta a ambos.  Si el grafo es dirigido la arista va del primer vertice al segundo.
+        Si alguno de los 2 vertices no existe se levanta una exepción. Si la arista que conecta a los vertices no existe se levanta una exepción.
+
+        Retorna el peso de dicha arista. 
+        """
         if id_1 not in self.vertices:
             raise ValueError("El vertice 1 no existe")
         if id_2 not in self.vertices:
@@ -117,23 +169,40 @@ class Grafo:
         return self.vertices[id_1].adyacentes[id_2]
 
     def obtener_vertices(self):
+        """
+        Devuelve una lista que contiene a los vertices pertenecientes al grafo.
+        """
         vertices = []
         for id in self.vertices.keys():
             vertices.append(id)
         return vertices
 
     def obtener_adyacentes(self, id):
+        """
+        Recibe por parametro un vertice y devuelve un diccionario con todos sus adyacentes, donde la clave es el vertice adyacente y el valor el peso de la arista que los conecta. 
+        Si no existe el vertice se levanta una exepción.
+        """
         if id not in self.vertices:
             raise ValueError("El vertice no existe")
         return self.vertices[id].adyacentes
     
     def obtener_cantidad_adyacentes(self, id):
+        """
+        Recibe por parametro un vertice y devuelve la cantidad de adyacentes de dicho vertice.
+        Si el vertice no existe se levanta una exepción.
+        """
         if id not in self.vertices:
             raise ValueError("El vertice no existe")
         return len(self.vertices[id].adyacentes)
 
     def obtener_cantidad_vertices(self):
+        """
+        Devuelve la cantidad de vertices que contiene el grafo.
+        """
         return self.cantidad_vertices
 
     def obtener_cantidad_aristas(self):
+        """
+        Devuelve la cantidad de aristas que contiene el grafo.
+        """
         return self.cantidad_aristas
